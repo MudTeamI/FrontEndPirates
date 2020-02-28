b275e399d96d61e6bc86d4d48b5f81a205341b1a
Shelby Diamond <Shelbydiamond518@gmail.com>
2020-02-28 00:22:09 -0700

    updated styles on home page.
    will work on login/registration styles next

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -13,12 +13,16 @@ const HomePage = () => {
       />
       <div className="title">
         <h2>Pillage and Plunder</h2>
-        <Link to="/login">
-          <p>Sign In</p>
-        </Link>
-        <Link to="/registration">
-          <p>Sign Up</p>
-        </Link>
+        <div className="paragraph">
+          <Link to="/login">
+            <p>Sign In</p>
+          </Link>
+        </div>
+        <div className="paragraph">
+          <Link to="/registration">
+            <p>Sign Up</p>
+          </Link>
+        </div>
       </div>
     </div>
   )
